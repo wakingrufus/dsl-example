67abc799f662258766bd2d36451b610885eee079
John Burns <wakingrufus@gmail.com>
2018-12-14 07:53:40 -0600

    Code clean up add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+#DSL Example
+This project is an example of extending the kotlin html and css DSLs to build a static website.
+You can find the companion article on [my site](https://wakingrufus.neocities.org/staticwebarticle.html).
+To run the demo, execute `./gradlew test` and view the file `build/output/index.html`
--- src/main/kotlin/com/github/wakingrufus/dslexample/CONTACT.kt
+++ src/main/kotlin/com/github/wakingrufus/dslexample/CONTACT.kt
@@ -6,77 +6,62 @@ import kotlinx.css.em
 import kotlinx.html.*
 
 @HtmlTagMarker
-class CONTACT{
+class CONTACT {
     var picture: String = ""
     var name: String = ""
     var email: String = ""
     var phone: String = ""
 
-    operator fun invoke(div: DIV){
+    operator fun invoke(div: DIV) {
         div.apply {
             div {
                 div {
-                    div {
+                    style = css {
+                        height = 3.em
+                        display = Display.inlineBlock
+                    }
+                    val imageBase64 = resourceAsBase64(this@CONTACT.picture)
+                    img(src = "data:image/png;base64, $imageBase64") {
                         style = css {
                             height = 3.em
-                            display = Display.inlineBlock
-                        }
-                        val imageBase64 = resourceAsBase64(this@CONTACT.picture)
-                        img(src = "data:image/png;base64, $imageBase64") {
-                            style = css {
-                                height = 3.em
-                                maxHeight = 3.em
-                            }
-                        }
-                    }
-                    div {
-                        style = css {
-                            display = Display.inlineBlock
-                        }
-                        span {
-                            style = css {
-                                display = Display.block
-                                fontWeight = FontWeight.bold
-                                fontSize = 1.17.em
-                            }
-                            +this@CONTACT.name
-                        }
-                        div {
-                            style = css {
-                                display = Display.inlineBlock
-                            }
-                            span {
-                                style = css {
-                                    display = Display.inlineBlock
-                                    width = 6.em
-                                }
-                                +"Email"
-                            }
-                            span {
-                                style = css {
-                                    display = Display.inlineBlock
-                                }
-                                a(href = "mailto://" + this@CONTACT.email) { +this@CONTACT.email }
-                            }
-                        }
-                        div {
-                            span {
-                                style = css {
-                                    display = Display.inlineBlock
-                                    width = 6.em
-                                }
-                                +"Phone"
-                            }
-                            span {
-                                style = css {
-                                    display = Display.inlineBlock
-                                }
-                                +this@CONTACT.phone
-                            }
+                            maxHeight = 3.em
                         }
                     }
                 }
+                div {
+                    style = css {
+                        display = Display.inlineBlock
+                    }
+                    span {
+                        style = css {
+                            display = Display.block
+                            fontWeight = FontWeight.bold
+                            fontSize = 1.17.em
+                        }
+                        +this@CONTACT.name
+                    }
+                    field("Email") { a(href = "mailto://" + this@CONTACT.email) { +this@CONTACT.email } }
+                    field("Phone") { +this@CONTACT.phone }
+                }
             }
+        }
+    }
+}
+
+fun DIV.field(name: String, value: SPAN.() -> Unit) {
+    div {
+        span {
+            style = css {
+                display = Display.inlineBlock
+                width = 6.em
+            }
+            +name
+        }
+        span {
+            style = css {
+                display = Display.inlineBlock
+            }
+            value(this)
         }
     }
 }
